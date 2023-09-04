6779975c69c4e18331ea8e53d99f5945916a9827
nguyen hai long <hailongavatar1@gmail.com>
2023-09-04 16:30:19 +0700

    deploy

--- style.js
+++ style.js
@@ -1,26 +1,12 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const box = document.querySelectorAll(".box");
-    const circle = document.querySelectorAll(".circle");
-    const text = document.querySelectorAll(".text");
-    box.forEach((item,key) => {
-        item.addEventListener("mouseenter",(e)=>{
-            let x = e.clientX;
-            let y = e.clientY;
-            text[key].style.opacity = 1;
-            text[key].style.visibility = "visible";
-            circle[key].style.top = `${y - item.getBoundingClientRect().top}px`;
-            circle[key].style.left = `${x - item.getBoundingClientRect().left}px`;
-            circle[key].style.transform = `translate(-50%,-50%) scale(25)`;
-        })
-        item.addEventListener("mouseleave",(e)=>{
-            circle[key] = document.querySelector(".circle");
-            let x = e.clientX;
-            let y = e.clientY;
-            text[key].style.opacity = 0;
-            text[key].style.visibility = "hidden";
-            circle[key].style.top = `${y - item.getBoundingClientRect().top}px`;
-            circle[key].style.left = `${x - item.getBoundingClientRect().left}px`;
-            circle[key].style.transform = `translate(-50%,-50%) scale(0)`;
-        })
+document.addEventListener("DOMContentLoaded",()=> {
+    const form = document.querySelector(".Login-box-form");
+    form.addEventListener("submit",(e)=> {
+        e.preventDefault();
+        let info = {
+            "Username":`${e.target.elements["username"].value}`,
+            "Password":`${e.target.elements["password"].value}`,
+            "Checkbox":`${document.querySelector("#checkbox").checked}`
+        }
+        console.log(info);
     })
 })
